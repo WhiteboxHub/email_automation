4d8c385216f20129923a493cf2bbfea538f29b96
mahendar06 <mahendargoud9866@gmail.com>
2025-04-09 10:56:16 -0700

    updated html template

--- HTMLTemplate.js
+++ HTMLTemplate.js
@@ -1,11 +1,11 @@
 
 
 const htmlTemplate = (name, experience, skill, location, visa, resumeLink, phone, linkedin, email) => `
-<p>Hello, Dear [Reciepent's name] </p>
+<p>Hello </p>
 <p>I hope this message finds you well</p>
 <p> My name is ${name} and, I am experienced in ${skill} with ${experience}. I am excited to share my profile with you. I have work authorization and am ready to contribute. I am available for <strong style="color:darkblue">C2C and C2H with fulltime W2 opportunities</strong>.</p>
 <p>I do not require visa sponsorship as I hold a <strong style="color:darkblue">${visa} status</strong>.</p>
-<p>If my this aligns with your current needs, please don't hesitate to reach out. Additionally, I would appreciate it if you could add my email to your daily distribution list:
+<p>If this aligns with your current needs, please don't hesitate to reach out. Additionally, I would appreciate it if you could add my email to your daily distribution list:
     <strong style="color:darkblue"><a href="mailto:${email}">${email}</a></strong>. Feel free to call me at <strong style="color:darkblue">${phone}</strong> or connect on <a href="${linkedin}">LinkedIn</a> for any further discussions.</p>
 <p>Best regards,</p>
 <p>${name}</p>
